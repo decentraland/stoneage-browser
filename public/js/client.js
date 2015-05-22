67956929e1725a840bc80164c9ccac2b7bd3b4ac
Esteban Ordano <eordano@gmail.com>
2015-05-22 16:36:49 -0300

    Starting client

---
+++ public/js/client.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var Mining = require('./mining');
+var Blockchain = require('./blockchain');
+
+function Client() {
+
+}
+
+Client.prototype.getState = function() {
+};
+
+module.exports = Client;
