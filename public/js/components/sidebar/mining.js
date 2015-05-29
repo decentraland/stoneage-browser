550c93043aa2441e7171bdb69460d3f73483f2ba
Esteban Ordano <eordano@gmail.com>
2015-05-29 13:37:42 -0300

    Toggle mining (requires decentraland-core update)

--- public/js/components/sidebar/mining.js
+++ public/js/components/sidebar/mining.js
@@ -3,11 +3,32 @@
 var React = require('react');
 
 var Mining = React.createClass({
+  getInitialState: function() {
+    return {
+      mining: this.props.mining.enableMining
+    };
+  },
+  switchMiner: function() {
+    this.props.mining.switchMining();
+    this.setState(this.getInitialState());
+  },
   render: function() {
+    var buttonText = this.props.mining.enableMining ? 'Disable' : 'Enable';
+    var mining = this.props.mining.enableMining ? 'Mining' : 'Not Mining';
+    var buttonClass = this.props.mining.enableMining ? 'btn-warning' : 'btn-success';
     return (
       <div>
         <h2>Status</h2>
         <ul>
+          <li>
+            <strong>{mining}</strong>
+            <button
+              onClick={this.switchMiner}
+              className="btn {buttonClass}"
+            >
+              {buttonText}
+            </button>
+          </li>
           <li><strong>Mining Target</strong>: {this.props.mining.target.x}, {this.props.mining.target.y}</li>
           <li><strong>Current Difficulty</strong>: {this.props.mining.difficulty}</li>
           <li><strong>Target Color</strong>: {this.props.mining.color}</li>
