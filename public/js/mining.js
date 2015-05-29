c3869472d9d28313453ad67ffa03308d84442c80
Esteban Ordano <eordano@gmail.com>
2015-05-29 12:37:27 -0300

    Mining on a web worker

--- public/js/mining.js
+++ public/js/mining.js
@@ -3,70 +3,89 @@
 var core = require('decentraland-core');
 var $ = core.util.preconditions;
 var _ = core.deps._;
+var events = require('events');
+var bufferUtils = core.util.buffer;
+var Hash = core.crypto.Hash;
 var Transaction = core.Transaction;
 var Miner = core.Miner;
 var Pos = require('./components/position');
+var Block = core.Block;
+
+var util = require('util');
+var inherits = util.inherits;
 
 function Mining (opts) {
   $.checkArgument(_.isObject(opts));
+  var self = this;
 
-  this.blockchain = opts.blockchain;
-  this.nonce = opts.nonce || 0;
+  this.worker = new Worker('./js/webworker.js');
+
+  this.worker.onmessage = function(event) {
+    self.emit('block', Block.fromString(event.data));
+  };
+  this.worker.onerror = function() {
+    console.log('error', arguments);
+  };
+
+  this.bits = opts.bits || 0x1f0fffff;
+  this.client = opts.client;
   this.publicKey = opts.publicKey;
   this.target = opts.target;
   this.color = opts.color;
-  this.txPool = opts.txPool;
-  this.callback = opts.callback;
+
+  this.mining = false;
 }
+inherits(Mining, events.EventEmitter);
 
 Mining.prototype.startMining = function() {
 
   var self = this;
   var opts = {};
+  opts.bits = this.bits;
+  opts.color = this.color;
+  opts.target = this.target;
+  opts.publicKey = this.publicKey.toString();
+  opts.previous = this.client.blockchain.getTipBlock().toString();
+  opts.txPool = this.client.txPool.map(function(tx) { return miner.addTransaction(tx); });
 
-  if (this.target === null) {
-    console.log('No target');
-    throw new Error('No target');
-  }
-
-  opts.coinbase = new Transaction()
-    .at(this.target.x, this.target.y)
-    .to(this.publicKey)
-    .colored(this.color);
-
-  opts.previous = this.blockchain.getTipBlock();
-  opts.time = Math.round(new Date() / 1000);
-  opts.bits = 0x1f0fffff;
-
-  var miner = this.miner = new Miner(opts);
-  this.txPool.map(function(tx) { return miner.addTransaction(tx); });
-
-  var newTarget = function(block) {
-    self.target = null;
-    var base = block.transactions[0].position;
-    _.shuffle(Pos.neighbors(base)).map(function(pos) {
-      if (!self.blockchain.pixels[Pos.posToString(pos)]) {
-        self.target = pos;
-      }
-    });
-  };
-
-  miner.on('block', function(block) {
-    newTarget(block);
-    if (self.target !== null) {
-      miner.newTip(block, new Transaction()
-        .at(self.target.x, self.target.y)
-        .to(self.publicKey)
-        .colored(self.color)
-      );
-    }
-    self.callback(block);
+  this.mining = true;
+  this.worker.postMessage({
+    type: 'MINE',
+    payload: opts
   });
-  miner.run();
+};
+
+Mining.prototype.setNewTarget = function(target) {
+  this.pause();
+  this.target = target;
+  this.startMining();
+};
+
+Mining.prototype.setNewColor = function(color) {
+  this.pause();
+  this.color = color;
+  this.startMining();
+};
+
+Mining.prototype.setNewPublicKey = function(publicKey) {
+  this.pause();
+  this.publicKey = publicKey;
+  this.startMining();
 };
 
 Mining.prototype.addTransaction = function(transaction) {
-  this.miner.addTransaction(transaction);
+  this.worker.postMessage({type: 'ADDTX', payload: transaction.toString()});
+};
+
+Mining.prototype.pause = function() {
+  this.worker.postMessage({
+    type: 'PAUSE'
+  });
+};
+Mining.prototype.resume = function() {
+  this.worker.postMessage({
+    type: 'RESUME'
+  });
 };
 
 module.exports = Mining;
